b741bb32d818f1ea25fe7640175007b54f8b1b23
chiayinin <chiayinin@gmail.com>
2022-09-07 19:51:43 +0800

    Update 2022-09-07T11:51:33.417Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,6 +1,36 @@
 "use strict";
 
-$(function () {
-  console.log('Hello Bootstrap5');
+// import Swiper JS
+var swiper = new Swiper('.mySwiper', {
+  // 參數設定
+  // autoplay: {
+  //     disableOnInteraction: false,
+  //     delay: 0
+  // },
+  // direction: "vertical", // 方向
+  loop: true,
+  // 循環
+  watchSlidesProgress: true,
+  speed: 3000,
+  slidesPerView: 4,
+  spaceBetween: 30,
+  centeredSlides: true,
+  // 分頁物件
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true
+  },
+  // RWD
+  breakpoints: {
+    480: {
+      slidesPerView: 1
+    },
+    767: {
+      slidesPerView: 2
+    },
+    996: {
+      slidesPerView: 4
+    }
+  }
 });
 //# sourceMappingURL=all.js.map
